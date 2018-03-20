f4e37cd3f279f7f9b3ee16b43187f29140624bbe
Usuario <Usuario@DESKTOP-702>
2018-03-20 14:44:23 +0100

    Añadimos Json

---
+++ EjerciciosBD/MainProgram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Data;
+
+namespace EjerciciosBD
+{
+    class MainProgram
+    {
+        public static readonly String PATHJSON = @"C:\Users\Usuario\Source\Repos\EjerciciosBD\EjerciciosBD\preguntas.json";
+
+        static void Main(string[] args)
+        {
+
+            createMenu(PreguntaSQL.fromJSON(File.ReadAllText(PATHJSON)));
+
+            /*using (var db = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Usuario\\Documents\\Pizzeria.mdf;Integrated Security=True;Connect Timeout=30"))
+             {
+                 db.Open();
+                 
+                 
+            }//Recordatorio -> llamada al dispose() de forma automática
+            
+            
+             var RepPizza = new RepPizzas(db.GetTable<Pizza>());
+             var RepIngrediente = new RepIngredientes(db.GetTable<Ingrediente>());
+
+             listarPizzas(RepPizza.getAll());*/
+
+            Console.WriteLine("Pulsa cualquier tecla para cerrar la aplicación");
+            Console.ReadKey();
+        }
+
+        static void listarPizzas(List<Pizza> listPizza)
+        {
+            foreach (var item in listPizza)
+            {
+                Console.WriteLine("Pizza: "+item.Name+", con id: "+item.Id+" y precio: "+item.getPrecio().ToString());
+            }
+        }
+
+        static void createMenu(List<PreguntaSQL> listPreguntas)
+        {
+            foreach (var item in listPreguntas)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }     
+}
---
+++ EjerciciosBD/PreguntaSQL.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosBD
+{
+    public class PreguntaSQL
+    {
+        public int Id { get; set; }
+        public String Pregunta { get; set; }
+
+        public PreguntaSQL(int id, String pregunta)
+        {
+            Id = id;
+            Pregunta = pregunta;
+        }
+
+        public override string ToString()
+        {
+            return Pregunta;
+        }
+
+        public static List<PreguntaSQL> fromJSON(String fileJson)
+        {
+            //TODO añadir excepción por archivo incorrecto
+            DataSet ds = JsonConvert.DeserializeObject<DataSet>(fileJson);
+            DataTable dt = ds.Tables["questions"];
+            var aux = dt.Select();
+            List<PreguntaSQL> listPreguntas = new List<PreguntaSQL>();
+            foreach (var item in aux)
+            {
+                listPreguntas.Add(new PreguntaSQL(int.Parse(item[0].ToString()), item[1].ToString()));
+            }
+            return listPreguntas;
+        }
+
+    }
+}
